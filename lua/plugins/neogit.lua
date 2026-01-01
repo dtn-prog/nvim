1a56bfadcd3a52a2e8132cd0350865dbbb944c69
dtn-prog <dtncdoe@gmail.com>
2026-01-01 23:44:08 +0700

    neogit

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,14 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    "sindrets/diffview.nvim", -- optional - Diff integration
+
+    "ibhagwan/fzf-lua",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gn", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+  },
+}
